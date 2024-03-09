8ab248ee88a6a9b1bad9c10c1a4bf30048954eb4
pwnaxe <biuro.norsk@gmail.com>
2024-03-09 19:47:58 +0100

    updated deleted routing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,9 @@
   } from '@smui/top-app-bar';
   import { writable } from 'svelte/store';
   import IconButton from '@smui/icon-button';
-  import Top from './components/top.svelte'
+  import Button, { Label } from '@smui/button';
+  import Card, { Content } from '@smui/card';
+	import Techlogos from './components/techlogos.svelte';
 
   let topAppBar: TopAppBar;
   const isDropdownOpen = writable(false);
@@ -16,6 +18,12 @@
   }
   function closeDropdown() {
     isDropdownOpen.set(false);
+  }
+
+  
+  let isVisible = true;
+  function closewin() {
+    isVisible = false;
   }
 </script>
 
@@ -29,9 +37,72 @@
   </Row>
 </TopAppBar>
 <AutoAdjust {topAppBar}>
-  <Top className="layout"/>
+  <div class="norskit-top">
+    <div class="text-container">
+      <h1>Odkryj przyszłość programowania z nami</h1>
+      <p>Z nami programowanie to nie tylko kod, to realizacja wizji nowoczesnej technologii. Dlaczego warto nas wybrać?</p>
+      <ul>
+        <li><strong>Innowacyjne rozwiązania AI:</strong> Stawiamy na sztuczną inteligencję, by dostarczać rozwiązania wyprzedzające czas.</li>
+        <li><strong>Efektywność kosztowa:</strong> Wysoka jakość w konkurencyjnej cenie. Dostajesz więcej, płacąc mniej.</li>
+        <li><strong>Ekspresowa realizacja:</strong> Twoje projekty są dla nas priorytetem. Realizacja w rekordowym tempie bez utraty na jakości.</li>
+      </ul>
+      <p>Nie pozwól, by ograniczenia tradycyjnego programowania hamowały Twój rozwój. <strong>Z nami osiągniesz więcej</strong>.</p>
+      <div class="button-container"> 
+        <Button class="layout" variant="raised">
+          <Label>Poproś o wycenę</Label>
+        </Button>
+        <Button class="layout" variant="raised">
+          <Label>Nasze projekty</Label>
+        </Button>      
+      </div>
+    </div>
+    <div class="card-display" class:isHidden={!isVisible}>
+      <Card>
+        <Content>
+          <h2>Przebudowa w toku!</h2>
+          <p>Jesteśmy w trakcie aktualizacji naszej strony do Svelte,<br> aby zapewnić jeszcze szybsze i bardziej efektywne doświadczenie.<br> Odwiedź nas wkrótce, aby zobaczyć nowości!</p>
+          <p>Masz pytania? <a href="mailto:biuro.norsk@gmail.com">Napisz do nas</a>.</p>
+          <Button class="layout" variant="raised" on:click={closewin}>
+            <Label>Zamknij okno</Label>
+          </Button>
+        </Content>
+      </Card>
+    </div>
+  </div>
+  <div>
+    <h1>Co robimy?</h1>
+    <p>W naszej ofercie znajdziesz szeroki wachlarz usług, które pomogą Ci w realizacji Twoich projektów. Sprawdź, co możemy dla Ciebie zrobić!</p>
+    <Techlogos />
+  </div>
 </AutoAdjust>
 </div>
 
 <style>
+  .norskit-top {
+    display: flex;
+    justify-content: flex-start;
+    align-items: center; 
+    height: 100vh;
+}
+
+  .text-container {
+    max-width: 50vw;
+    padding-left: 5vw;
+  }
+  .button-container {
+  display: flex;
+  justify-content: flex-start;
+  gap: 20px;
+  padding-top: 20px;
+}
+
+.card-display {
+  display: flex;
+  position: absolute;
+  right: 0;
+  color: black; 
+}
+.isHidden {
+    display: none;
+  }
 </style>
