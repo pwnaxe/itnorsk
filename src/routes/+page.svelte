b94fa2ae0286fa4bf526c8754a777624fad50035
pwnaxe <biuro.norsk@gmail.com>
2024-03-23 21:43:55 +0100

    added animation css, removed three

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,6 @@
     AutoAdjust,
   } from '@smui/top-app-bar';
   import IconButton from '@smui/icon-button';
-  import Stormtrooper from './components/stormtrooper.svelte';
   import Menu from '@smui/menu';
   import List, { Item, Text } from '@smui/list';
   import LayoutGrid, { Cell } from '@smui/layout-grid';
@@ -45,39 +44,54 @@
       <div class="text-container">
         <h1>NORSK <br> HUB</h1>
       </div>
-      <div class="stormtrooper">
-        <Stormtrooper />
-      </div>
       <LayoutGrid>
         <Cell>
-          <div class="cellcard animate__headShake">
-            <h2>Outsource Developers</h2>
-            <p>Explore cutting-edge technology with our team of expert developers available for hire. <br><br> From advanced software solutions to innovative IT projects, we provide the skilled workforce you need to bring your vision to life.</p>
-          </div>
+          <a href="/contact" class="cell-link">
+            <div class="cellcard animate__bounceInUp">
+              <h2>Outsource Developers</h2>
+              <p>Explore cutting-edge technology with our team of expert developers available for hire. <br><br> From advanced software solutions to innovative IT projects, we provide the skilled workforce you need to bring your vision to life.</p>            
+            </div>
+          </a>
         </Cell>
         <Cell>
-          <div class="cellcard animate__headShake">
-            <h2>Web, Mobile, Desktop App Development</h2>
-            <p>Develop seamless, scalable, and robust applications across all platforms. Our expertise spans from creating engaging websites to building sophisticated mobile and desktop apps, integrating modern UI/UX designs with state-of-the-art functionalities to ensure an exceptional user experience.</p>
-          </div>
+          <a href="/contact" class="cell-link">
+            <div class="cellcard animate__bounceInUp">
+              <h2>Web, Mobile, Desktop App Development</h2>
+              <p>Develop seamless, scalable, and robust applications across all platforms. Our expertise spans from creating engaging websites to building sophisticated mobile and desktop apps, integrating modern UI/UX designs with state-of-the-art functionalities to ensure an exceptional user experience.</p>
+            </div>
+          </a>
         </Cell>
         <Cell>
-          <div class="cellcard animate__headShake">
-            <h2>Our Technologies</h2>
-            <p>List and describe the key technologies and tools your team utilizes in development projects, such as React, Angular, Vue for front-end, Node.js, Python for back-end, Flutter, React Native for mobile apps, Docker for containerization, Kubernetes for orchestration, AWS, Azure for cloud services, and more.</p>
-          </div>
+          <a href="/contact" class="cell-link">
+            <div class="cellcard animate__bounceInUp">
+              <h2>Our Technologies</h2>
+              <p>List and describe the key technologies and tools your team utilizes in development projects, such as React, Angular, Vue for front-end, Node.js, Python for back-end, Flutter, React Native for mobile apps, Docker for containerization, Kubernetes for orchestration, AWS, Azure for cloud services, and more.</p>
+            </div>
+          </a>
         </Cell>
         <Cell>
-          <div class="cellcard animate__headShake">
-            <h2>IT Infrastructure Maintenance</h2>
-            <p>Ensure the reliability and efficiency of your IT infrastructure with our comprehensive maintenance services. <br><br>From routine checks to emergency repairs, our team provides the support you need to keep your operations running smoothly.</p>
-          </div>
+          <a href="/contact" class="cell-link">
+            <div class="cellcard animate__bounceInUp">
+              <h2>IT Infrastructure Maintenance</h2>
+              <p>Ensure the reliability and efficiency of your IT infrastructure with our comprehensive maintenance services. <br><br>From routine checks to emergency repairs, our team provides the support you need to keep your operations running smoothly.</p>
+            </div>
+          </a>
         </Cell>
         <Cell>
-          <div class="cellcard animate__headShake">
-            <h2>Payment Wall</h2>
-            <p>Fast and Secure payment wall to our clients. Pay for Our invoices with your favorite payment method. No added fees.</p>
-          </div>
+          <a href="/contact" class="cell-link">
+            <div class="cellcard animate__bounceInUp">
+              <h2>Payment Wall</h2>
+              <p>Fast and Secure payment wall to our clients. Pay for Our invoices with your favorite payment method. No added fees.</p>
+            </div>
+          </a>
+        </Cell>
+        <Cell>
+          <a href="/contact" class="cell-link">
+            <div class="cellcard animate__bounceInUp">
+              <h2>Contact Us</h2>
+              <p>Dont want to waste Your time anymore?<br> You had our curiosity, now You have our attention.</p>
+            </div>
+          </a>
         </Cell>
     </LayoutGrid>
     </AutoAdjust>
@@ -105,6 +119,16 @@
   flex-direction: column;
   justify-content: center;
   box-sizing: border-box;
+  cursor: pointer;
+}
+
+.cell-link {
+  text-decoration: none;
+  color: inherit;
+}
+
+.cellcard:hover {
+  box-shadow: 0 6px 12px rgba(0,0,0,0.9);
 }
 
 @media (max-width: 600px) {
@@ -114,16 +138,7 @@
   }
 }
 
-.stormtrooper {
-  position: absolute;
-  top: 5%;
-  right: 0;
-  width: 50%;
-  height: 100%;
-}
-
-.animate__headShake {
-  animation-duration: 10s;
-  animation-iteration-count: infinite;
+.animate__bounceInUp {
+  animation-duration: 1s;
 }
 </style>
