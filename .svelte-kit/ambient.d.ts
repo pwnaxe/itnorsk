0d73c7a475fe1b471a29a168d4650a4c8668a919
pwnaxe <biuro.norsk@gmail.com>
2024-03-23 16:38:25 +0100

    front fixes
    added css animations for better ux
    changed cards for layout grid for responsivity

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -41,7 +41,7 @@ declare module '$env/static/private' {
 	export const ComSpec: string;
 	export const DriverData: string;
 	export const EDITOR: string;
-	export const EFC_7444: string;
+	export const EFC_7496: string;
 	export const FPS_BROWSER_APP_PROFILE_STRING: string;
 	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
@@ -162,7 +162,7 @@ declare module '$env/dynamic/private' {
 		ComSpec: string;
 		DriverData: string;
 		EDITOR: string;
-		EFC_7444: string;
+		EFC_7496: string;
 		FPS_BROWSER_APP_PROFILE_STRING: string;
 		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,19 +6,11 @@
     AutoAdjust,
   } from '@smui/top-app-bar';
   import IconButton from '@smui/icon-button';
-  import Card, { Content as CardContent } from '@smui/card';
   import Stormtrooper from './components/stormtrooper.svelte';
-  import Drawer, {
-    AppContent,
-    Header,
-    Title as DrawerTitle,
-    Subtitle,
-    Content as DrawerContent,
-  } from '@smui/drawer';
   import Menu from '@smui/menu';
   import List, { Item, Text } from '@smui/list';
-  import Button, { Label } from '@smui/button';
-  
+  import LayoutGrid, { Cell } from '@smui/layout-grid';
+  import 'animate.css';
 
   let topAppBar: TopAppBar;
   let menu: Menu;
@@ -49,7 +41,6 @@
       </Section>
     </Row>
   </TopAppBar>
-  <AppContent>
     <AutoAdjust {topAppBar}>
       <div class="text-container">
         <h1>NORSK <br> HUB</h1>
@@ -57,75 +48,82 @@
       <div class="stormtrooper">
         <Stormtrooper />
       </div>
-      <div class="cards-container">
-        <Card>
-          <CardContent>
+      <LayoutGrid>
+        <Cell>
+          <div class="cellcard animate__headShake">
             <h2>Outsource Developers</h2>
             <p>Explore cutting-edge technology with our team of expert developers available for hire. <br><br> From advanced software solutions to innovative IT projects, we provide the skilled workforce you need to bring your vision to life.</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
+          </div>
+        </Cell>
+        <Cell>
+          <div class="cellcard animate__headShake">
             <h2>Web, Mobile, Desktop App Development</h2>
             <p>Develop seamless, scalable, and robust applications across all platforms. Our expertise spans from creating engaging websites to building sophisticated mobile and desktop apps, integrating modern UI/UX designs with state-of-the-art functionalities to ensure an exceptional user experience.</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
+          </div>
+        </Cell>
+        <Cell>
+          <div class="cellcard animate__headShake">
             <h2>Our Technologies</h2>
             <p>List and describe the key technologies and tools your team utilizes in development projects, such as React, Angular, Vue for front-end, Node.js, Python for back-end, Flutter, React Native for mobile apps, Docker for containerization, Kubernetes for orchestration, AWS, Azure for cloud services, and more.</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
+          </div>
+        </Cell>
+        <Cell>
+          <div class="cellcard animate__headShake">
             <h2>IT Infrastructure Maintenance</h2>
             <p>Ensure the reliability and efficiency of your IT infrastructure with our comprehensive maintenance services. <br><br>From routine checks to emergency repairs, our team provides the support you need to keep your operations running smoothly.</p>
-          </CardContent>
-        </Card>
-        <Card>
-          <CardContent>
+          </div>
+        </Cell>
+        <Cell>
+          <div class="cellcard animate__headShake">
             <h2>Payment Wall</h2>
             <p>Fast and Secure payment wall to our clients. Pay for Our invoices with your favorite payment method. No added fees.</p>
-          </CardContent>
-        </Card>
-      </div>
+          </div>
+        </Cell>
+    </LayoutGrid>
     </AutoAdjust>
-  </AppContent>
 </div>
 
 <style>  
-  .text-container {
-    padding-left: 15vw;
-    padding-top: 15vh;
-  }
-  
-  .text-container h1 {
-    font-size: 3.5em;
-    margin: 0;
-  }
+ .text-container {
+  padding-left: 15vw;
+  padding-top: 15vh;
+}
 
-  .cards-container {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    align-items: center;
-    align-content: space-around;
-    padding-top: 25vh;
-  }
+.text-container h1 {
+  font-size: 3.5em;
+  margin: 0;
+}
 
-  .cards-container > * {
-    flex: 1; /* Pozwala kartom na rozciąganie */
-    margin: 10px; /* Dodaje margines dookoła kart */
-    min-width: 250px; /* Minimalna szerokość karty, poniżej której karty zaczną się wrapować */
-    max-width: 30%; /* Maksymalna szerokość karty */
-  }
 
-  .stormtrooper {
-    position: absolute;
-    top: 5%;
-    right: 0;
-    width: 50%;
-    height: 100
+.cellcard {
+  padding: 20px;
+  margin: 10px;
+  text-align: center;
+  box-shadow: 0 4px 8px rgba(0,0,0,0.6);
+  border-radius: 14px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  box-sizing: border-box;
+}
+
+@media (max-width: 600px) {
+  .cellcard {
+    width: 100%;
+    margin: 10px 0;
   }
+}
+
+.stormtrooper {
+  position: absolute;
+  top: 5%;
+  right: 0;
+  width: 50%;
+  height: 100%;
+}
+
+.animate__headShake {
+  animation-duration: 10s;
+  animation-iteration-count: infinite;
+}
 </style>
